77eb79eb5753169e74d14b75826c9a49eba12feb
Owen Williams <owen@zoocha.com>
2017-01-18 22:27:52 +0000

    init 3

--- drush/local.aliases.drushrc.php
+++ drush/local.aliases.drushrc.php
@@ -1,20 +1,5 @@
 <?php
 
-/**
- * After cloning the repo, copy this file and name it local.aliases.drushrc.php
- * That's a gitignored file so you won't be able to push it.
- *
- * Make sure you set your SSH user for sites in the array below.
- * Normally on Opsworks stacks your username will be the same across all stacks and aws accounts.
- *
- * If a site is not on opsworks then the username will probably be 'ubuntu'
- * In that case you will probably need to specify the path to the ssh key as well,
- * This is still experimental but for those servers you can add your ssh_path below
- * and it should work. (the user will be specified in the alias file)
- * Example: /home/zoocha/.ssh/
- * (include the slash at the end)
- */
-
 // $local_vars = array(
 //   'user' => 'CHANGE_ME'
 //   'ssh_path' => 'CHANGE_ME'
